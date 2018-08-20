f0fab7cd0d8bd45ba8240bbf0c7a3a7399b2e675
Guilherme Waess <guilherme.waess@gmail.com>
2018-08-20 23:49:07 +0200

    Removing unused components

--- src/router.js
+++ src/router.js
@@ -1,6 +1,6 @@
 import Vue from 'vue';
 import Router from 'vue-router';
-import Home2 from './views/Home2.vue';
+import Home from './views/Home.vue';
 
 Vue.use(Router);
 
@@ -9,7 +9,7 @@ export default new Router({
     {
       path: '/',
       name: 'home',
-      component: Home2,
+      component: Home,
     },
   ],
   mode: 'history',
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,35 +1,28 @@
 <template>
-  <v-container fluid>
-    <v-slide-y-transition mode="out-in">
-      <v-layout column align-center>
-        <img src="@/assets/logo.png" alt="Vuetify.js" class="mb-5">
-        <blockquote>
-          &#8220;First, solve the problem. Then, write the code.&#8221;
-          <footer>
-            <small>
-              <em>&mdash;John Johnson</em>
-            </small>
-          </footer>
-        </blockquote>
+  <div>
+    <PageHeader></PageHeader>
+    <v-container fluid
+                 fill-height>
+      <v-layout align-center
+                justify-center>
+        <v-flex xs12
+                md6>
+          <VenuesContainer></VenuesContainer>
+        </v-flex>
       </v-layout>
-    </v-slide-y-transition>
-  </v-container>
+    </v-container>
+  </div>
 </template>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h1, h2 {
-  font-weight: normal;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
+<script>
+import VenuesContainer from './../containers/VenuesContainer.vue';
+import PageHeader from './../components/PageHeader.vue';
+
+export default {
+  name: 'Home',
+  components: {
+    VenuesContainer,
+    PageHeader,
+  },
+};
+</script>
